841c3f1326c46ea501d25c17f56cdfe2cf51373d
androcpp <georgeandronache.cpp@gmail.com>
2022-02-21 23:02:43 +0200

    Refactored merged LoginScreen

--- screens/Login.js
+++ screens/Login.js
@@ -1,5 +1,5 @@
 import { View, Text, StyleSheet, ImageBackground, Image, TextInput, Dimensions } from 'react-native'
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 
 import bgImage from '../assets/images/loginBackground.jpg'
 import logoIcon from '../assets/icons/rnlogo.png'
@@ -7,66 +7,61 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import { TouchableOpacity } from 'react-native'
 
 const { width: WIDTH } = Dimensions.get('window')
-export default class Login extends Component {
-  constructor() {
-    super()
-    this.state = {
-      showPass: true,
-      press: false
-    }
-  }
+export default function Login() {
 
-  showPass = () => {
-    if(this.state.press == false) {
-      this.setState({showPass: false, press: true})
+  [press, setPress] = useState(false);
+  [showPass, setShowPass] = useState(true);
+  
+  showPasswd = () => {
+    if(press == false) {
+      setPress(true);
+      setShowPass(false);
     }else{
-      this.setState({showPass: true, press: false})
+      setPress(false);
+      setShowPass(true);
     }
   }
 
-  render() {
-    return (
-      <ImageBackground source={bgImage} style={styles.backgroundContainer}>
-        <View style={styles.logoContainer}>
-          <Image source={logoIcon} style={styles.logo}/>
-          <Text style={styles.logoText}>MazeFM</Text>
-        </View>
-  
-        <View style={styles.inputContainer}>
-          <Icon name={'ios-person-outline'} size = {28} color={'rgba(255,255,255, 0.7)'}
-            style={styles.inputIcon}/>
-          <TextInput 
-            style={styles.input}
-            placeholder={'Username'}
-            placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
-            underlineColorAndroid='transparent'/>
-        </View>
-  
-        <View style={styles.inputContainer}>
-          <Icon name={'ios-lock-outline'} size = {28} color={'rgba(255,255,255, 0.7)'}
-            style={styles.inputIcon}/>
-          <TextInput 
-            style={styles.input}
-            placeholder={'Password'}
-            secureTextEntry={this.state.showPass}
-            placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
-            underlineColorAndroid='transparent'/>
-  
-          <TouchableOpacity style={styles.btnEye}
-            onPress={this.showPass.bind(this)}>
-            <Icon name={this.state.press == false ? 'ios-eye-outline' : 'ios-eye-off-outline'} size={26} color={'rgba(255,255,255, 0.7)'}/>
-          </TouchableOpacity>
-  
-        </View>
-  
-        <TouchableOpacity style={styles.btnLogin}>
-            <Text style={styles.text}>Login</Text>
-          </TouchableOpacity>
-  
-      </ImageBackground>
-    )
-  }
-  
+  return (
+    <ImageBackground source={bgImage} style={styles.backgroundContainer}>
+      <View style={styles.logoContainer}>
+        <Image source={logoIcon} style={styles.logo}/>
+        <Text style={styles.logoText}>MazeFM</Text>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Icon name={'ios-person-outline'} size = {28} color={'rgba(255,255,255, 0.7)'}
+          style={styles.inputIcon}/>
+        <TextInput 
+          style={styles.input}
+          placeholder={'Username'}
+          placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
+          underlineColorAndroid='transparent'/>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Icon name={'lock-open-outline'} size = {28} color={'rgba(255,255,255, 0.7)'}
+          style={styles.inputIcon}/>
+        <TextInput 
+          style={styles.input}
+          placeholder={'Password'}
+          secureTextEntry={showPass}
+          placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
+          underlineColorAndroid='transparent'/>
+
+        <TouchableOpacity style={styles.btnEye}
+          onPress={() => showPasswd()}>
+          <Icon name={press == false ? 'ios-eye-outline' : 'ios-eye-off-outline'} size={26} color={'rgba(255,255,255, 0.7)'}/>
+        </TouchableOpacity>
+
+      </View>
+
+      <TouchableOpacity style={styles.btnLogin}>
+          <Text style={styles.text}>Login</Text>
+        </TouchableOpacity>
+
+    </ImageBackground>
+  )
 }
 
 const styles = StyleSheet.create({
